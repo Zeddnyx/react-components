6ff83d8138e99c7bf8aa66f333780130f3bbf1d6
Zeddnyx <zeddnyx08@gmail.com>
2023-08-05 15:04:09 +0700

    refactor: add memo

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -12,20 +12,20 @@ interface Props {
 function Input({ type, placeholder, value, onChange, logo, isError }: Props) {
   return (
     <div className="relative flex flex-col gap-1">
-      <div className="relative flex items-center">
-        {logo && (
-          <img src={logo} alt="input logo" className="w-4 h-4 absolute ml-2" />
-        )}
-        <input
-          type={type}
-          placeholder={placeholder}
-          value={value}
-          onChange={onChange}
-          className={`${
-            logo ? "pl-7 " : ""
-          } focus:ring-0 outline-none w-full text-sm border p-1 rounded-md`}
-        />
-      </div>
+      <div className="relative flex items-center"> 
+        {logo && ( 
+          <img src={logo} alt="input logo" className="w-4 h-4 absolute ml-2" /> 
+        )} 
+        <input 
+          type={type} 
+          placeholder={placeholder} 
+          value={value} 
+          onChange={onChange} 
+          className={`${ 
+            logo ? "pl-7 " : "" 
+          } focus:ring-0 outline-none w-full text-sm border p-1 rounded-md`} 
+        /> 
+      </div> 
       <p className="text-start text-xs text-red-500">{isError}</p>
     </div>
   );
