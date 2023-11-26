a401af4b30058d25d4bf938a7a304772bba523a9
Zeddnyx <zeddnyx08@gmail.com>
2023-11-26 17:02:59 +0700

    fix(form suggest auto close suggest)

--- src/pages/form/components/form-search-suggest/index.tsx
+++ src/pages/form/components/form-search-suggest/index.tsx
@@ -8,7 +8,7 @@ import { getDataNormal } from "~/utils/fetch";
 export default function Index() {
   const [search, setSearch] = useState("");
   const [data, setData] = useState([]);
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
   const searchKey = useDebounce(search, 500);
 
   useEffect(() => {
@@ -20,31 +20,36 @@ export default function Index() {
       });
   }, [searchKey]);
 
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    setIsOpen(false);
+  };
+
   return (
     <div>
-      <Input
-        value={search}
-        type="text"
-        placeholder="Search"
-        onChange={(e) => setSearch(e.target.value)}
-      />
+      <form onSubmit={handleSubmit}>
+        <Input
+          value={search}
+          type="text"
+          placeholder="Search"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </form>
       <div className="h-28 py-4">
         <div className="overflow-y-auto h-full">
-          {isOpen ||
-            (data?.length != 0 &&
-              data?.map((item: any) => (
-                <div
-                  key={item.id}
-                  className="cursor-pointer hover:bg-slate-300 p-2"
-                  onMouseOut={() => setIsOpen(false)}
-                  onClick={() => {
-                    setSearch(item.title);
-                    setIsOpen(false);
-                  }}
-                >
-                  {item.title}
-                </div>
-              )))}
+          {isOpen &&
+            data?.map((item: any) => (
+              <div
+                key={item.id}
+                className="cursor-pointer hover:bg-slate-300 p-2"
+                onClick={() => {
+                  setSearch(item.title);
+                  setIsOpen(false);
+                }}
+              >
+                {item.title}
+              </div>
+            ))}
         </div>
       </div>
     </div>
